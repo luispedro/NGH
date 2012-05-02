ae435cc2a392355c88efe1ec438f85b466727fe7
Luis Pedro Coelho <luis@luispedro.org>
2012-05-02 19:25:15 +0100

    DOC Document public functions

--- Data/NGH/Formats/Embl.hs
+++ Data/NGH/Formats/Embl.hs
@@ -9,14 +9,22 @@ import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
 
-readSeq :: L.ByteString -> Maybe L.ByteString
+-- | read a sequence from an EMBL formatted file
+-- If there is no sequence, then `Nothing` is returned
+readSeq :: L.ByteString -- ^ The whole file (as a lazy string)
+        -> Maybe L.ByteString -- ^ A sequence if found
 readSeq = readSeq' . L8.lines
 
 readSeq' [] = Nothing
 readSeq' (s:ss)
-    | "SQ" `L.isPrefixOf` s = (Just . L.fromChunks . map getSeq) ss
+    | "SQ" `L.isPrefixOf` s = (Just . L.fromChunks . map getSeq . beforeend) ss
     | otherwise = readSeq' ss
 
 getSeq = S.filter isACTG . S.concat . L.toChunks
 isACTG = isJust . (`L.elemIndex` "actgACTG")
+
+beforeend [] = []
+beforeend (s:ss)
+    | "\\\\" `L.isPrefixOf` s = []
+    | otherwise = (s:beforeend ss)
 
--- Data/NGH/Formats/Fasta.hs
+++ Data/NGH/Formats/Fasta.hs
@@ -8,7 +8,11 @@ import qualified Data.ByteString as S
 import qualified Data.ByteString.Char8 () -- import IsString instance
 import qualified Data.ByteString.Lazy as L
 
-writeSeq :: Int -> L.ByteString -> L.ByteString -> L.ByteString
+-- |writeSeq formats a sequence in Fast-A format
+writeSeq :: Int -- ^ Width of the lines to use
+            -> L.ByteString -- ^ Header
+            -> L.ByteString -- ^ The DNA sequence
+            -> L.ByteString -- ^ The output
 writeSeq lw header s = L.fromChunks (["> ", S.concat $ L.toChunks header, "\n"] ++ breakup lw s)
 
 breakup :: Int -> L.ByteString -> [S.ByteString]
