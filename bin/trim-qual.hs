25d04a36e9c7422c0db699a502ff927ea9c2efcb
Luis Pedro Coelho <luis@luispedro.org>
2012-06-14 17:01:44 +0100

    ENH Add maximum # of N parameter

--- bin/trim-qual.hs
+++ bin/trim-qual.hs
@@ -12,9 +12,9 @@ import Data.Maybe
 import Data.List (isSuffixOf)
 import Data.Conduit.Zlib (ungzip)
 import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Util as CU
 import qualified Data.Conduit.Binary as CB -- bytes
 import Data.NGH.Trim
-import ParBuffer
 
 
 data TrimCmd = TrimCmd
@@ -22,13 +22,15 @@ data TrimCmd = TrimCmd
                 , output :: String
                 , minLength :: Int
                 , filterAs :: Bool
+                , maxNs :: Int
                 } deriving (Eq, Show, Data, Typeable)
 
 trimcmds = TrimCmd
-            { input = "-" &= argPos 0
-            , output = "-" &= argPos 1
+            { input = "-" &= argPos 0 -- &= help "Input file"
+            , output = "-" &= argPos 1 -- &= help "Output file"
             , minLength = 18  &= help "Minimum read length"
             , filterAs = True &= help "Filter A-only sequences"
+            , maxNs = -1 &= help "Maximum number of Ns (default: -1, interpreted as ∞)"
             } &=
             verbosity &=
             summary sumtext &=
@@ -40,15 +42,18 @@ trimcmds = TrimCmd
 
 -- mayunzip :: (Monad m, MonadUnsafeIO m, MonadThrow m) => String -> Pipe Void S.ByteString m () -> Pipe Void S.ByteString m ()
 mayunzip finput c
-    | "gz" `isSuffixOf` finput = (c $= parBufferN 32 ungzip)
+    | "gz" `isSuffixOf` finput = (c $= ungzip)
     | otherwise = c
 
-isgood :: Int -> Bool ->  DNAwQuality -> (Bool,DNAwQuality)
-isgood mL fA x = (isgood' x, x)
+isgood :: Int -> Bool -> Int ->  DNAwQuality -> (Bool,DNAwQuality)
+isgood mL fA mNs x = (isgood' x, x)
     where
         isgood' x = ((S.length $ dna_seq x) > mL)
                         &&
                     ((not fA) || (not $ allAs x))
+                        &&
+                    ((mNs < 0) || (S8.count 'N' $ dna_seq x) <= mNs)
+
 allAs = isNothing . S8.findIndex (/= 'A') . dna_seq
 
 strict :: L.ByteString -> S.ByteString
@@ -56,15 +61,15 @@ strict = S.concat . L.toChunks
 
 main :: IO ()
 main = do
-    TrimCmd finput foutput mL fAs <- cmdArgs trimcmds
+    TrimCmd finput foutput mL fAs mNs <- cmdArgs trimcmds
     (_,(g,t)) <- runResourceT $
         (mayunzip finput $ CB.sourceFile finput)
         =$= (fastQConduit illumina)
-        =$= CL.map (isgood mL fAs. trimLS 30)
+        =$= CL.map (isgood mL fAs mNs . trimLS 30)
         $$ (CL.filter fst
             =$= CL.map (strict . fastQwrite . snd)
             =$ CB.sinkFile foutput)
-                    `CL.zipSinks`
+                    `CU.zipSinks`
             (CL.map fst
                =$ CL.fold (\(!g,!t) v -> (if v then (g+1) else g, t+1)) (0.0 :: Double,0.0 :: Double))
     putStrLn $ concat ["Processed ", show t, " sequences."]
