56c7f21f24c7c926566187e2840e917794dab690
Luis Pedro Coelho <luis@luispedro.org>
2012-06-22 14:48:50 +0100

    ENH Compile embl-to-fasta

--- bin/embl-to-fasta.hs
+++ bin/embl-to-fasta.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+import System.Console.CmdArgs
 import Data.Maybe
 import System.Environment
 import qualified Data.ByteString.Lazy as L
@@ -7,10 +9,21 @@ import Data.NGH.Formats.Fasta
 import Data.NGH.Formats.Embl
 import Codec.Compression.GZip
 
+data E2FCmd = E2FCmd
+        { input :: String
+        , output :: String
+        } deriving (Eq, Show, Data, Typeable)
+
+e2fcmds = E2FCmd
+        { input = "-" &= argPos 0 &= typ "Input-file"
+        , output = "-" &= argPos 1 &= typ "Output-file"
+        } &=
+        verbosity &=
+        details ["Convert Embl to Fasta files"]
 
 main :: IO ()
 main = do
-    [inputf,outputf] <- getArgs
+    E2FCmd inputf outputf <- cmdArgs e2fcmds
     let header = L.concat [L8.pack "converted from ", L8.pack inputf]
     (L.readFile inputf) >>= (L.writeFile outputf . writeSeq 72 header . fromJust . readSeq . decompress)
     
