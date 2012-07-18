51a8d74f2e0ef418a928005735a33beb16e3b4b4
Luis Pedro Coelho <luis@luispedro.org>
2012-07-18 14:43:30 +0100

    ENH L8.readInt should be faster

--- Data/NGH/Formats/Sam.hs
+++ Data/NGH/Formats/Sam.hs
@@ -19,18 +19,23 @@ readAlignments' (l:ls)
     | L8.head l == '@' = readAlignments' ls
     | otherwise = (readSamLine l:readAlignments' ls)
 
+
+readInt b = case L8.readInt b of
+    Just (v,_) -> v
+    _ -> error $ concat ["Expected int, got ", show b]
+
 readSamLine :: L.ByteString -> SamLine
 readSamLine line = case L8.split '\t' line of
     (tk0:tk1:tk2:tk3:tk4:tk5:tk6:tk7:tk8:tk9:tk10:_) -> SamLine
                 (strict tk0)
-                (read $ L8.unpack tk1)
+                (readInt tk1)
                 (strict tk2)
-                (read $ L8.unpack tk3)
-                (read $ L8.unpack tk4)
+                (readInt tk3)
+                (readInt tk4)
                 (strict tk5)
                 (strict tk6)
-                (read $ L8.unpack tk7)
-                (read $ L8.unpack tk8)
+                (readInt tk7)
+                (readInt tk8)
                 (strict tk9)
                 (strict tk10)
     tokens -> error $ concat ["Expected 11 tokens, only got ", show $ length tokens,"\n\t\tLine was '", show line, "'"]
